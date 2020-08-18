f53895e9817de479c2d6671409fcdde54d5d45c5
Mathias Brodala <mbrodala@pagemachine.de>
2020-08-18 18:03:12 +0200

    [TASK] Add npmcopy task configuration

---
+++ .grunt/npmcopy.js
@@ -0,0 +1,16 @@
+module.exports = {
+  css: {
+    options: {
+      destPrefix: '<%= paths.resources.public %>/Css',
+    },
+    files: {
+    },
+  },
+  js: {
+    options: {
+      destPrefix: '<%= paths.resources.public %>/JavaScript',
+    },
+    files: {
+    },
+  }
+};
